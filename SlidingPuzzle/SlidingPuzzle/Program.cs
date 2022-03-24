bf1586089decb8f214080bd121df8850f294a2e7
Jonathan Jonsson <jonsson.h.jonathan@gmail.com>
2022-03-24 12:54:58 +0100

    Add: Started building getNeighbour in State

--- SlidingPuzzle/SlidingPuzzle/Program.cs
+++ SlidingPuzzle/SlidingPuzzle/Program.cs
@@ -3,10 +3,12 @@ using SlidingPuzzle;
 
 State state = new();
 
-// Console.WriteLine(state.GetCell(1,0).targetCellNumber);
+Console.WriteLine(state.GetCell(1,0).targetCellNumber);
 
  state.PrintTargetState();
 Console.WriteLine();
 state.PrintCurrentState();
+
+state.GetNeighbour();
 
  
--- SlidingPuzzle/SlidingPuzzle/State.cs
+++ SlidingPuzzle/SlidingPuzzle/State.cs
@@ -1,12 +1,12 @@
-﻿namespace SlidingPuzzle;
+﻿using System.Windows.Input;
+namespace SlidingPuzzle;
 
-public class State
+public class State //Node basically!
 {
-	public Cell[] grid = new Cell[9];
-	public int gridWidth = 3;
-	private bool winState = false;
+	public Cell[] Grid = new Cell[9];
+	public int GridWidth = 3;
 
-	private List<int> numberPool = new List<int>()
+	private readonly List<int> numberPool = new()
 	{
 		1,
 		2,
@@ -15,34 +15,29 @@ public class State
 		5,
 		6,
 		7,
-		8
+		8,
+		-1
 	};
-
-	public Cell GetCell(int x, int y)
-	{
-		return grid[x + y*gridWidth];
-	}
-
-	private Random random = new Random();
 
 	public State()
 	{
-		int x = 1;
+		var x = 1;
+		var shuffledNumbers = numberPool.OrderBy(a => Guid.NewGuid()).ToList();
 
-		for (int i = 0; i < grid.Length; i++)
+		for (var i = 0; i < Grid.Length; i++)
 		{
-			grid[i] = new Cell();
-
-			grid[i].targetCellNumber = x;
-			var randomPoolNumber = random.Next(0, numberPool.Count);
-			grid[i].currentCellNumber = numberPool[randomPoolNumber]; //TODO: change for array so duplicates cant be selected
-			
+			Grid[i] = new Cell
+			{
+				targetCellNumber = x,
+				currentCellNumber = shuffledNumbers[i]
+			};
 			x++;
-
 		}
+	}
 
-
-
+	public Cell GetCell(int x, int y)
+	{
+		return Grid[x + y*GridWidth];
 	}
 
 	public void PrintCurrentState()
@@ -50,9 +45,9 @@ public class State
 		int x = 0, y = 0;
 		Console.WriteLine();
 		Console.WriteLine("Current state (-1 = empty slot): ");
-		for (int i = 0; i < grid.Length; i++)
+
+		for (var i = 0; i < Grid.Length; i++)
 		{
-			 
 			if (x > 2)
 			{
 				y++;
@@ -64,21 +59,22 @@ public class State
 
 			Console.Write(GetCell(x, y).currentCellNumber + " | ");
 			x++;
-		}	
+		}
 	}
-	
+
 	public void PrintTargetState()
 	{
 		int x = 0, y = 0;
 		Console.WriteLine();
 		Console.WriteLine("Target state (-1 = empty slot): ");
-		for (int i = 0; i < grid.Length; i++)
+
+		for (var i = 0; i < Grid.Length; i++)
 		{
-			if (i == grid.Length - 1)
+			if (i == Grid.Length - 1)
 			{
-				grid[i].targetCellNumber = -1;
-				
+				Grid[i].targetCellNumber = -1;
 			}
+
 			if (x > 2)
 			{
 				y++;
@@ -95,7 +91,7 @@ public class State
 
 	public bool CheckWinState()
 	{
-		foreach (var cell in grid)
+		foreach (var cell in Grid)
 		{
 			if (cell.currentCellNumber != cell.targetCellNumber)
 			{
@@ -104,5 +100,39 @@ public class State
 		}
 
 		return true;
+	}
+
+	public void GetNeighbour()
+	// public IEnumerable<State> GetNeighbour()
+	{
+		var allowed = false;
+		var inputKey = new ConsoleKey();
+		
+		while (!allowed)
+		{
+			Console.WriteLine("Use arrows to reach targetState by moving to -1 (representing the empty block)");
+			inputKey = Console.ReadKey().Key;
+			if (inputKey == ConsoleKey.UpArrow || inputKey == ConsoleKey.DownArrow || inputKey == ConsoleKey.LeftArrow || inputKey == ConsoleKey.RightArrow)
+				allowed = true;
+		}
+		
+		if (inputKey == ConsoleKey.UpArrow)
+		{
+			
+		}
+		if (inputKey == ConsoleKey.DownArrow)
+		{
+			
+		}
+		if (inputKey == ConsoleKey.LeftArrow)
+		{
+			
+		}
+		if (inputKey == ConsoleKey.RightArrow)
+		{
+			
+		}
+		
+		
 	}
 }
